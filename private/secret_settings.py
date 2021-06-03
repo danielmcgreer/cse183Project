a90ac3b7a4f7aa8672deabfc09bf1ed16c030789
Daniel McGreer <daniel.mcgreer@gmail.com>
2021-06-03 14:27:30 -0700

    removed my keys

--- private/secret_settings.py
+++
@@ -1,7 +0,0 @@
-# single sign on Google (will be used if provided)
-OAUTH2GOOGLE_CLIENT_ID = "325029556998-lldoe659ta01egnni35gt5g6v7qn7tum.apps.googleusercontent.com"
-OAUTH2GOOGLE_CLIENT_SECRET = "yDZpllwcMq3OJjoVzJ_UwVgl"
-DB_CONNECTION = "course-rater-315521:us-west1:course-rater-db-1"
-DB_PASSWORD = "cse183courserater"
-DB_USER = "appuser"
-DB_NAME = "db"
